80b59c3a4904acbaa028eef40fbce07a8503b039
Dan <decisivedaniel@proton.me>
2025-06-05 14:54:40 +0200

    Main function layed out

--- bitmap/src/main.rs
+++ bitmap/src/main.rs
@@ -1,30 +1,66 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    let mut white_noise = String::new();
+    let row_white_noise = "WWWWWWWWBBBBBBBBWWWWBBBBWWBBBWW";
+    let message = String::from("Reading Dilbert strips or encoding Elbonian messages are not good excuses for failing the XBC009 final exam.");
+
+    let response = user_selection();
+
+    create_wn_pattern(&mut white_noise, row_white_noise);
+
+    if response {
+        encrypt(white_noise.as_str(), message.as_str());
+    } else {
+        decrypt(white_noise.as_str());
+    }
 }
 
-fn get_message (message: &str) {
-    
+fn encrypt(white_noise: &str, message: &str) {
+    let packed_message = get_message(message);
+    let encoded_message = run_length_encode(&packed_message);
+    create_image(&encoded_message, white_noise);
+}
+
+fn decrypt(white_noise: &str) {
+    let mut encrypted_white_noise = String::new();
+    read_image(&mut encrypted_white_noise, white_noise);
+    let decoded_message = run_length_decode(&encrypted_white_noise);
+    let trimmed_message = retrieve_message(&decoded_message);
+    println!("Message {trimmed_message}");
+}
+
+fn user_selection() -> bool {
+    println!("enter 0 for encryption, enter 2 for decryption\n");
+    let mut buf = String::new();
+    match io::stdin().read_line(&mut buf) {
+        Ok(x) => {
+            return buf.parse::<bool>().unwrap();
+        }
+        Err(error) => println!("Error: {error}")
+    };
+    return true;
+}
+
+fn get_message (message: &str) -> String {
+    let lead = "XXXXXXXXBBBBCCCCOOOO000099999999";
+    let package = lead.to_string() + message + lead;
+    return package;
 } 
 
-fn retrieve_message(message: &str) {
+fn retrieve_message(message: &str) -> String {
 
 }
 
-fn run_length_encode(message: &str) {
+fn run_length_encode(message: &str) -> String {
 
 }
 
-fn run_length_decode(encodedMessage: &str) {
+fn run_length_decode(encoded_message: &str) -> String {
     
 }
 
-fn create_wn_pattern(whiteNoise: &str, rowWhiteNoise: &str){
-
-}
-
-fn encrypt_message(message: &str, messageLength: u64, whiteNoise: &str){
+fn create_wn_pattern(white_noise: &mut String, row_white_noise: &str){
 
 }
 
